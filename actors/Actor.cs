dab40a393023ffdc2a8aba6ffb45fd53845ad362
AlexGarbus <alex.garbus@outlook.com>
2022-09-30 21:48:57 -0400

    Player punching

--- actors/Actor.cs
+++ actors/Actor.cs
@@ -5,8 +5,22 @@ namespace LudumDare51.Actors
 {
     public class Actor : Area2D
     {
+        protected enum State
+        {
+            IDLE,
+            DODGE,
+            PUNCH,
+            HIT,
+        }
+
         [Export(PropertyHint.Range, "1,100,or_greater")]
         private int _maxHealth = 10;
+
+        [Export(PropertyHint.Range, "0,100,or_greater")]
+        private float _punchDistance;
+
+        [Export(PropertyHint.Range, "0,100,or_greater")]
+        private float _punchTime;
 
         public int Health
         {
@@ -17,31 +31,45 @@ namespace LudumDare51.Actors
             }
         }
 
-        private int _health;
+        protected State _state = State.IDLE;
 
-        //protected AnimationNodeStateMachinePlayback _animationState;
-        //protected AnimationTree _animationTree;
         protected AnimationPlayer _animationPlayer;
 
         protected Position2D _fistPivot;
 
         protected Vector2 _idlePosition;
 
+        private int _health;
+
         public override void _Ready()
         {
-            //_animationTree = GetNode<AnimationTree>("%AnimationTree");
             _animationPlayer = GetNode<AnimationPlayer>("%AnimationPlayer");
             _fistPivot = GetNode<Position2D>("%FistPivot");
 
             _idlePosition = Position;
             Health = _maxHealth;
-
-            //_animationState = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
+        }
+        
+        protected void Move(Vector2 displacement, float time)
+        {
+            SceneTreeTween tween = GetTree().CreateTween();
+            tween.TweenProperty(this, "position", _idlePosition + displacement, time);
+            tween.TweenCallback(_animationPlayer, "play", new Godot.Collections.Array("idle", time));
+            tween.TweenProperty(this, "position", _idlePosition, time);
+            tween.TweenCallback(this, "Idle");
         }
 
-        protected void Punch()
+        protected void Idle()
         {
-            // TODO: Implement
+            _state = State.IDLE;
+        }
+
+        protected void Punch(Vector2 direction)
+        {
+            _state = State.PUNCH;
+            _animationPlayer.Play("punch", _punchTime);
+
+            Move(_punchDistance * direction, _punchTime);
         }
     }
 }
--- actors/player/Player.cs
+++ actors/player/Player.cs
@@ -5,21 +5,11 @@ namespace LudumDare51.Actors
 {
     public class Player : Actor
     {
-        private enum State
-        {
-            IDLE,
-            DODGE,
-            PUNCH,
-            HIT,
-        }
-
         [Export(PropertyHint.Range, "0,100,or_greater")]
         private float _dodgeDistance;
 
         [Export(PropertyHint.Range, "0,100,or_greater")]
         private float _dodgeTime;
-        
-        private State _state = State.IDLE;
 
         public override void _UnhandledInput(InputEvent @event)
         {
@@ -30,21 +20,16 @@ namespace LudumDare51.Actors
 
             if (@event.IsActionPressed("dodge_left"))
             {
-                Dodge(new Vector2(-1, 0));
+                Dodge(Vector2.Left);
             }
             else if (@event.IsActionPressed("dodge_right"))
             {
-                Dodge(new Vector2(1, 0));
+                Dodge(Vector2.Right);
             }
             else if (@event.IsActionPressed("punch"))
             {
-                Punch();
+                Punch(Vector2.Up);
             }
-        }
-
-        private void Idle()
-        {
-            _state = State.IDLE;
         }
 
         private void Dodge(Vector2 direction)
@@ -59,11 +44,7 @@ namespace LudumDare51.Actors
             }
             _fistPivot.Scale = pivotScale;
 
-            SceneTreeTween tween = GetTree().CreateTween();
-            tween.TweenProperty(this, "position", _idlePosition + direction * _dodgeDistance, _dodgeTime);
-            tween.TweenCallback(_animationPlayer, "play", new Godot.Collections.Array("idle", _dodgeTime));
-            tween.TweenProperty(this, "position", _idlePosition, _dodgeTime);
-            tween.TweenCallback(this, "Idle");
+            Move(_dodgeDistance * direction, _dodgeTime);
         }
     }
 }
