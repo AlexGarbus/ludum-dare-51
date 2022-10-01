bbe2c20f148b3535c4053c9eea45a576a6862b61
AlexGarbus <alex.garbus@outlook.com>
2022-10-01 12:39:47 -0400

    Round label

--- user_interface/fight_display/FightDisplay.cs
+++ user_interface/fight_display/FightDisplay.cs
@@ -1,4 +1,5 @@
 using LudumDare51.Actors;
+using LudumDare51.AutoLoad;
 using Godot;
 using System;
 
@@ -6,15 +7,22 @@ namespace LudumDare51.UserInterface
 {
     public class FightDisplay : Control
     {
+        private FightData _fightData;
+
+        private FightTimer _fightTimer;
+
         private HealthBar _playerHealthBar;
         private HealthBar _enemyHealthBar;
-        private FightTimer _fightTimer;
+
+        private Label _roundLabel;
 
         public override void _Ready()
         {
+            _fightData = GetNode<FightData>(AutoLoadPaths.FIGHT_DATA_PATH);
+            _fightTimer = GetNode<FightTimer>("%FightTimer");
             _playerHealthBar = GetNode<HealthBar>("%PlayerHealthBar");
             _enemyHealthBar = GetNode<HealthBar>("%EnemyHealthBar");
-            _fightTimer = GetNode<FightTimer>("%FightTimer");
+            _roundLabel = GetNode<Label>("%RoundLabel");
         }
 
         public void Initialize(Player player, Enemy enemy, SceneTreeTimer fightTimer)
@@ -22,6 +30,8 @@ namespace LudumDare51.UserInterface
             _playerHealthBar.Initialize(player);
             _enemyHealthBar.Initialize(enemy);
             _fightTimer.Initialize(fightTimer);
+
+            _roundLabel.Text = $"Round {_fightData.Round}";
         }
     }
 }
