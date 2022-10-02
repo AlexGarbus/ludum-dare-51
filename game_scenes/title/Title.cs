8afa9fd11fe20c19d439de0386a76dd7cf02e100
AlexGarbus <alex.garbus@outlook.com>
2022-10-02 17:40:44 -0400

    Added title sound delay to avoid cutting out in HTML

---
+++ game_scenes/title/Title.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+namespace LudumDare51.GameScenes
+{
+    public class Title : Control
+    {
+        private AudioStreamPlayer _titleSound;
+
+        public override void _Ready()
+        {
+            _titleSound = GetNode<AudioStreamPlayer>("%TitleSound");
+        }
+
+        private void OnTitleSoundDelayTimeout()
+        {
+            _titleSound.Play();
+        }
+    }
+}
